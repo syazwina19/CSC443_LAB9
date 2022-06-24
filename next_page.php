52e3de23ec445d4b7bdf65ed1e2f9837bb942bd5
syazwina19 <2020951065@student.uitm.edu.my>
2022-06-25 00:46:00 +0800

    commit

--- next_page.php
+++ next_page.php
@@ -2,12 +2,17 @@
     session_start();
     echo "<html>";
 
+    $sid = session_id();
+    echo "Session ID returned by session_id(): ".$sid."\n";
+    $sid = $ID;
+    echo "Session ID returned by SID: ".$sid."\n";
+
     $myLogin = $_SESSION["myLogin"];
     echo "Value of myLogin has been retrieved: ".$myLogin."\n";
-
     $myColor = $_SESSION["myColor"];
     echo "Value of myColor has been retrieved: ".$myColor."\n";
 
     echo "</html>\n";
 
 ?>
+
